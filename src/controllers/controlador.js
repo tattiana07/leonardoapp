0f185cf4a3df7c3e4151de568e0554531f1c97e8
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-07-12 17:24:46 -0500

    Se crea formulario

--- src/controllers/controlador.js
+++ src/controllers/controlador.js
@@ -1,3 +1,15 @@
+let boton=document.getElementById("botonChat")
+boton.addEventListener("click",function(){
+    let cajaChat=document.getElementById("cajaChat")
+
+    if(cajaChat.style.display=="none" || cajaChat.style.display==""){
+        cajaChat.style.display="block"
+    }else{
+        cajaChat.style.display="none"
+    }
+})
+
+
 function cambiarCaja(){
     let boton=document.getElementById("botonChat")
     let cajaChat=document.getElementById("cajaChat")
